7d2ca9f6eb21191316f07a4c9d242fe00fcb77f1
Diego Alan <diegoalan07@gmail.com>
2026-05-23 15:58:21 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,16 +1,21 @@
 import { test, expect } from '@playwright/test'
 
+//AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  //Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
+  //Act
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-
   await page.getByTestId('search-order-id').fill('VLO-HJO7UV') 
   await page.getByTestId('search-order-button').click()
+
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible() 
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-HJO7UV')
   await expect(page.getByTestId('order-result-status')).toBeVisible()
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
-})
+}) //end of test
